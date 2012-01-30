28d1ec1effa53dee8e7f8e034230d140561ae7a6
Andy Boyle <andyjcboyle@gmail.com>
2012-01-30 16:07:22 +0000

    A few starting points

--- src/net/java/examples/generics/GenericExamplesExecutor.java
+++ src/net/java/examples/generics/GenericExamplesExecutor.java
@@ -1,7 +1,25 @@
 package net.java.examples.generics;
 
-public class GenericExamplesExecutor {
-	public static void main(String[] args) {
+import net.java.examples.generics.example1.typevariable.Mouse;
+import net.java.examples.generics.example1.typevariable.Trap;
 
+public class GenericExamplesExecutor {
+
+	public static void main(String[] args) {
+		example1_theTypeVariable();
 	}
+
+	private static void example1_theTypeVariable() {
+		Trap<Mouse> mouseTrap = new Trap<Mouse>(new Mouse("Mickey"));
+		mouseTrap.snare(new Mouse("Minnie"));
+		Mouse aMouse = mouseTrap.release();
+		System.out.println("Trapped a Mouse: " + aMouse);
+		mouseTrap.snare(new Mouse("Mighty"));
+		
+		System.out.println("\n\n--- A list of all things caught.\n");
+		for ( Mouse mouse : mouseTrap.allBeenTrappedSoFar()) {
+		  System.out.println(mouse);
+		}
+	}
+	
 }
