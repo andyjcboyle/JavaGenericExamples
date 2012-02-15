c0a2590d06e9c7c31cbec34696e2c508170a6598
Andy Boyle <andyjcboyle@googlemail.com>
2012-02-15 21:56:00 +0000

    A bit of tidying

--- src/net/java/examples/generics/writingGenericClasses/example1/typevariable/Trap.java
+++ src/net/java/examples/generics/writingGenericClasses/example1/typevariable/Trap.java
@@ -17,17 +17,18 @@ public class Trap<T> {
 
 	static {
 		System.out.println("Trap static initialiser 1");
-
 		// NOTE: We cannot refer to a the generic variable 'trapped' within a
 		// 'static' method because there is only ONE version of Trap class at
 		// runtime. Hence it makes no sense to be able to refer to an instance's
-		// type version.
+		// type version. i.e. if we try to change "trapped" and
+		// "allTrappedByThisInstance" would get compile error
+		
 		// If there were different classes at runtime per generic instantiation
 		// then this would be possible. e.g. like C++'s Templates ( I assume,
 		// though I couldn't find any statements that say so specifically).
 
-		// System.out.println("Trap the static init 2 : " +
-		// trapped.getClass().getSimpleName());
+		// System.out.println("Trap the static init 2 : "
+		// + trapped.getClass().getSimpleName());
 
 		// NOTE: For the same reason as above we cannot refer to the Type
 		// Variable 'T' - i.e. don't know what 'T' is at runtime so can't be
@@ -56,7 +57,7 @@ public class Trap<T> {
 		}
 		allTrappedByThisInstance = new ArrayList<T>();
 		System.out
-				.println("Trap instance initialser 3 : Created list to hold all trapped");
+				.println("Trap instance initialiser 3 : Created list to hold all trapped");
 	}
 
 	public void snare(T trapped) {
